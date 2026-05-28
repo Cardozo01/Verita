c9d16f91d8f0f0cb2af35534d39c8b05693c567b
Cardozo01 <gabriel.cardozo@ads.uniceplac.edu.br>
2026-05-27 23:37:48 -0300

    fix: adicionando vercel.json para correcao de rotas

--- verita-frontend/app/page.tsx
+++ verita-frontend/app/page.tsx
@@ -22,7 +22,7 @@ const traduzirBackend = (texto: string) => {
     .replace("high", "alto");
 };
 
-export default function page() {
+export default function Home() {
   const [barcode, setBarcode] = useState('');
   const [product, setProduct] = useState<ProductData | null>(null);
   const [loading, setLoading] = useState(false);
